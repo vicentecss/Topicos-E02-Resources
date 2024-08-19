a9e3321017f9de43abfbdf6ded7f2345477b78d0
vicentecss <vicentecss@hotmail.com>
2024-08-19 09:13:32 -0300

    Extração dos valores de dimensão

--- app/src/main/java/br/ufmg/coltec/olamundo/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/olamundo/MainActivity.java
@@ -1,14 +1,12 @@
 package br.ufmg.coltec.olamundo;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.view.Gravity.CENTER;
 
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import static android.view.Gravity.CENTER;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +16,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnSurprise = findViewById(R.id.btn_surprise);
-        btnSurprise.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast toast = Toast.makeText(MainActivity.this, R.string.toast, Toast.LENGTH_LONG);
-                toast.setGravity(CENTER, 0, 0);
-                toast.show();
-            }
+        btnSurprise.setOnClickListener(view -> {
+            Toast toast = Toast.makeText(MainActivity.this, R.string.toast, Toast.LENGTH_LONG);
+            toast.setGravity(CENTER, 0, 0);
+            toast.show();
         });
     }
 }
